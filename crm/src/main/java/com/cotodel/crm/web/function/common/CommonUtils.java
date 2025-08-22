7ec4691cbb134bfac154c3fbf95296072ce1ee60
Anish Mokashi <anish.mokashi@cotodel.com>
2025-08-22 15:38:26 +0530

    changed otp api endpoints

--- crm/src/main/java/com/cotodel/crm/web/function/common/CommonUtils.java
+++ crm/src/main/java/com/cotodel/crm/web/function/common/CommonUtils.java
@@ -6,9 +6,11 @@ public interface CommonUtils {
 	public static String tokenUrl="/tokenService/Api/get/access-token";
 	public static String sendOtp = "/userServices/Api/getOtp";
 	public static String otpWithOutRegister = "/userServices/Api/get/otpWithOutRegister";
-	public static String sendOtpNew = "/userServices/Api/getOtpNew";
+	//public static String sendOtpNew = "/userServices/Api/getOtpNew";
+	public static String sendOtpNew = "/userServices/Api/crm/getOtp";
 	public static String resendOtpNew = "/userServices/Api/getOtpResend";
-	public static String verifyOtpNew = "/userServices/Api/verifyOtpNew";
+	//public static String verifyOtpNew = "/userServices/Api/verifyOtpNew";
+	public static String verifyOtpNew = "/userServices/Api/crm/verifyOtp";
 	public static String verifyOtpWithOutRegister = "/userServices/Api/get/verifyOtpWithOutRegister";
 	public static String verifyOtp = "/userServices/Api/verifyOtp";
 
